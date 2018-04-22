e35d2b740b75dfefcbc934fc776dc0a524eefbd7
Dustin Vandehey <dustinvandehey@gmail.com>
2018-04-22 12:44:51 -0700

    admin controller test

---
+++ AdminController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlueRibbonsReview.Models;
+using BlueRibbonsReview.ViewModels;
+using Microsoft.AspNet.Identity;
+
+namespace BlueRibbonsReview.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            List<AdminViewModel> AdminViewModels = new List<AdminViewModel>();
+            IEnumerable<ApplicationUser> ApplicationUsers = db.Users.ToList();
+            foreach(ApplicationUser user in ApplicationUsers)
+            {
+                AdminViewModel AdminUser = new AdminViewModel(user);
+                AdminViewModels.Add(AdminUser);
+            } 
+            return View(AdminViewModels);
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
